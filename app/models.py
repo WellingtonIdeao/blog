43de4c550ca01e245ff30995dd9687886f9083cc
Wellington Ideão <wellington.ideao@gmail.com>
2020-10-05 22:30:16 -0300

    fix Post model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 # Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.datetime_safe import datetime
 
 
 class Categoria(models.Model):
@@ -11,10 +12,12 @@ class Categoria(models.Model):
 
 
 class Post(models.Model):
-    titulo = models.CharField(max_length=50, null=False)
+    titulo = models.CharField(max_length=100, null=False)
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
-    descricao = models.TextField(null=False)
+    descricao = models.CharField(max_length=200, null=False)
+    conteudo = models.TextField(null=False)
     categorias = models.ManyToManyField(Categoria)
+    data_cadastro = models.DateTimeField(default=datetime.now, editable=False, null=False)
 
     def __str__(self):
         return self.titulo
